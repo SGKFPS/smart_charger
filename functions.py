d0ce5f7aa38b4fda00cfabc13c6697c47e52e07d
st-FPS <69518928+st-FPS@users.noreply.github.com>
2020-09-16 14:49:29 +0100

    Clean single day algorithm

--- functions.py
+++ functions.py
@@ -55,44 +55,6 @@ def dumb_charging_no(journeys, eprice): # TODO am I using this?
     profiles['Electricity_costs'] = profiles['Site_output'] * profiles['unit_rate_excl_vat']
     return profiles
 
-# Create function for one vehicle, in one day
-def single_BAU_schedule(journeys, day, vehicle, eprice):
-    return_time = journeys.loc[(day, vehicle),'End_Time_of_Route']
-    return_datetime = dt.datetime.combine(day, return_time)
-    required_charge = journeys.loc[(day, vehicle),'Energy_Required']
-    depart_time = journeys.loc[(day + dt.timedelta(days=1), vehicle),'Start_Time_of_Route']
-    depart_datetime = dt.datetime.combine(day + dt.timedelta(days=1), depart_time)
-    #print(depart_datetime)
-    #print('Return:', return_datetime, "\nDepart:", depart_datetime, '\nRequired charge:', required_charge)
-    mask = ( (eprice['from'] >= dt.datetime.combine(day, gv.CHAR_ST)) 
-    & (eprice['from'] < dt.datetime.combine(day + dt.timedelta(days=1), gv.CHAR_ST)))
-    single_profile = eprice[mask].copy()
-    timesidx = single_profile.index
-    single_profile[gv.Power_output[vehicle]] = 0
-    single_profile[gv.SOC[vehicle]] = gv.BATTERY_CAPACITY - required_charge
-    prev_idx = timesidx[0]
-    for idx in timesidx:
-        if single_profile.loc[idx,'from'] + gv.TIME_INT <= return_datetime:
-            single_profile.loc[idx,gv.Power_output[vehicle]] = 0
-        elif single_profile.loc[idx,'from'] <= return_datetime:
-            time_charging = (return_datetime - single_profile.loc[idx,'from']).total_seconds()
-            single_profile.loc[idx,gv.Power_output[vehicle]] = time_charging * 3.5 / (gv.TIME_INT.total_seconds())
-        elif gv.BATTERY_CAPACITY - single_profile.loc[prev_idx,gv.SOC[vehicle]] > gv.CHARGER_POWER/2:
-            single_profile.loc[idx,gv.Power_output[vehicle]] = gv.CHARGER_POWER / 2 #FIXME generalise for any time interval
-        elif gv.BATTERY_CAPACITY - single_profile.loc[prev_idx,gv.SOC[vehicle]] > 0:
-            single_profile.loc[idx,gv.Power_output[vehicle]] = gv.BATTERY_CAPACITY - single_profile.loc[prev_idx,gv.SOC[vehicle]]
-        single_profile.loc[idx,gv.SOC[vehicle]] = gv.BATTERY_CAPACITY - required_charge + single_profile[gv.Power_output[vehicle]].sum()
-        prev_idx = idx  # FIXME initialise this earlier
-    return single_profile
-    
-# Get list of times in a day
-def create_timelist(): #FIXME so that it's always a numpy array
-    intervals = int(dt.timedelta(days=1) / gv.TIME_INT)
-    time_array = [gv.CHAR_ST_DELTA] * intervals
-    for i in range(intervals):
-        time_array[i] += gv.TIME_INT * i
-    return np.asarray(time_array)
-
 # Create a function to get daily data
 def get_daily_data(journeys,day):
     daily_df = journeys.loc[(day)].copy()
